1331b43e78b72bf4352cf34829b7e17371ca1467
roonlafisa <roonlafisa@gmail.com>
2021-06-18 05:21:24 -0700

    comments more

--- index.py
+++ index.py
@@ -4,13 +4,13 @@ upperRange = [20, 200, 2000, 20000, 200000, 200000]
 # Test Input List
 testInput = [15, 105, 1005, 10005, 100005, 100005]
 def checkRange(indelowerRange, num):
-   # using comaparision operator
+# using comaparision operator
    if lowerRange[indelowerRange] <= num <= upperRange[indelowerRange]:
       print('The value {} is in range ({}, {})'.format(num, lowerRange[indelowerRange], upperRange[indelowerRange]))
    else:
       print('The value {} is not in range ({}, {})'.format(num, lowerRange[indelowerRange], upperRange[indelowerRange]))
 
-
+#send values to comparison uperator
 for i, val in enumerate(testInput):
   print ('The input no is {} and the value is {}'.format(i , val))
   checkRange(i, val)
