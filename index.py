b61b2577b3b617f7e2821380132ccbf772cfd44f
roonlafisa <roonlafisa@gmail.com>
2021-06-18 01:50:17 -0700

    sample edit

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 # Given range
-X = [10, 100, 1000, 10000, 100000, 100000]
+XX = [10, 100, 1000, 10000, 100000, 100000]
 Y = [20, 200, 2000, 20000, 200000, 200000]
 # Test Input List
 testInput = [15, 105, 1005, 10005, 100005, 100005]
@@ -9,8 +9,8 @@ def checkRange(index, num):
       print('The value {} is in range ({}, {})'.format(num, X[index], Y[index]))
    else:
       print('The value {} is not in range ({}, {})'.format(num, X[index], Y[index]))
-      
-      
+
+
 for i, val in enumerate(testInput):
   print ('The input no is {} and the value is {}'.format(i , val))
   checkRange(i, val)
