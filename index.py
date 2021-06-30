14d5e02f7daa1f8171fe207c4ecf9ec8f80c03d6
roonlafisa <roonlafisa@gmail.com>
2021-06-30 01:53:48 -0700

    complete beta version

--- index.py
+++ index.py
@@ -1,16 +1,16 @@
-# Given range
-lowerRange = [10, 100, 1000, 10000, 100000, 100000]
-upperRange = [20, 200, 2000, 20000, 200000, 200000]
-# Test Input List
-testInput = [15, 105, 1005, 10005, 100005, 100005]
-def checkRange(index, num):
-# using comaparision operator
-   if lowerRange[index] <= num <= upperRange[index]:
-      print('The value {} is in range ({}, {})'.format(num, lowerRange[indelowerRange], upperRange[indelowerRange]))
-   else:
-      print('The value {} is not in range ({}, {})'.format(num, lowerRange[indelowerRange], upperRange[indelowerRange]))
+from Tkinter import *
 
-#send values to comparison uperator
-for i, val in enumerate(testInput):
-  print ('The input no is {} and the value is {}'.format(i , val))
-  checkRange(i, val)
+root = Tk()
+x = IntVar()
+
+
+def test():
+    global x
+    x.set(x.get() + 1)
+
+label_1 = Label(root, text=x.get(), textvariable = x)
+button_1 = Button(root, text='Click', command=test)
+button_1.grid(row=0, column=0)
+label_1.grid(row=0, column=1)
+
+root.mainloop()
--- inputValues.py
+++ inputValues.py
@@ -1,10 +1,19 @@
 from tkinter import *
 from tkinter import messagebox
 from tkinter.ttk import *
+import math
 
+def instructions():
+    #instructions
+    instruc = Toplevel(window)
+    instruc.title("Results")
+    instruc.geometry("600x200")
+    lblins1 = Label(instruc, text="Step1: Input your reference data and select percentage tolerance range. Hit 'set tolerance'")
+    lblins1.grid(column=0, row=1)
+    lblins2 = Label(instruc, text="Step2: Input your input values and hit 'measure button'")
+    lblins2.grid(column=0, row=3)
 
-
-def clicked():
+def set_tolerance():
 # ----do an empty label check
 #calculation:
 
@@ -23,78 +32,130 @@ def clicked():
   upperlimit3 = value3 + float(combo3.get())*value3/100
 
   value4 = float(txt4.get())
-  lowerlimit4 = value3 - float(combo4.get())*value4/100
-  upperlimit4 = value3 + float(combo4.get())*value4/100
+  lowerlimit4 = value4 - float(combo4.get())*value4/100
+  upperlimit4 = value4 + float(combo4.get())*value4/100
 
   value = [value1, value2, value3, value4]
   lowerlimit = [lowerlimit1, lowerlimit2, lowerlimit3, lowerlimit4]
   upperlimit = [upperlimit1, upperlimit2, upperlimit3, upperlimit4]
 
-  messagebox.showinfo('Message', 'Complete')
-  window.destroy()
+#  if (math.isnan(txt4.get())):
+#      messagebox.showinfo('Message', 'error')
+#      window.destroy()
 
-  for i, val in enumerate(value):
-    print ('The input no is {} and the value is {}'.format(i , val))
-    checkRange(i, val)
 
-def checkRange(index, num):
-# using comaparision operator
-   if lowerlimit[index] <= num <= upperlimit[index]:
-      print('The value {} is in range ({}, {})'.format(num, lowerlimit[index], upperlimit[index]))
-   else:
-      print('The value {} is not in range ({}, {})'.format(num, lowerlimit[index], upperlimit[index]))
+def checkRange():
+    global index, input, checkeddata
+    checkeddata=["red", "red", "red", "red"]
+    # set inputs
+    testin1 = float(txtin1.get())
+    testin2 = float(txtin2.get())
+    testin3 = float(txtin3.get())
+    testin4 = float(txtin4.get())
+    input = [testin1, testin2, testin3, testin4]
+
+    for index in range(len(input)):
+        if lowerlimit[index] <= input[index] <= upperlimit[index]:
+            checkeddata[index] = "green"
+            print(index)
+        else:
+
+            continue
+
+    newWindow = Toplevel(window)
+    newWindow.title("Results")
+    newWindow.geometry("200x200")
+    lblout1a = Label(newWindow, text="Input 1: ",)
+    lblout2a = Label(newWindow, text="Input 2: ")
+    lblout3a = Label(newWindow, text="Input 3: ")
+    lblout4a = Label(newWindow, text="Input 4: ")
+    lblout1b = Label(newWindow, text=checkeddata[0])
+    lblout2b = Label(newWindow, text=checkeddata[1])
+    lblout3b = Label(newWindow, text=checkeddata[2])
+    lblout4b = Label(newWindow, text=checkeddata[3])
+    lblout1a.grid(column=1, row=0)
+    lblout2a.grid(column=1, row=1)
+    lblout3a.grid(column=1, row=2)
+    lblout4a.grid(column=1, row=3)
+    lblout1b.grid(column=2, row=0)
+    lblout2b.grid(column=2, row=1)
+    lblout3b.grid(column=2, row=2)
+    lblout4b.grid(column=2, row=3)
+
+    button = Button(newWindow, text = "Click and Quit", command = newWindow.destroy)
+    button.grid(column=2, row=8)
+
+
+
+
+
+
 
 
 # complete code
 
 window = Tk()
-window.title("Input range for testing")
-window.geometry('700x300')
+window.title("Data Log QC Semi-Automation")
+window.geometry('1100x300')
 
-# input 1
+
+# value 1
 lbl1a = Label(window, text="Reference for input 1 ")
 txt1 = Entry()
 txt1.insert(END, '10')
 txt1.pack()
-lbl1b = Label(window, text="     Percentage range ")
+lbl1b = Label(window, text="     Percentage tolerance ")
 combo1 = Combobox(window)
 combo1['values']= (1, 5, 10, 15, 20)
 combo1.current(1) #set the selected item
 
-# input 2
+# value 2
 lbl2a = Label(window, text="Reference for input 2 ")
 txt2 = Entry()
 txt2.insert(END, '100')
 txt2.pack()
-lbl2b = Label(window, text="     Percentage range ")
+lbl2b = Label(window, text="     Percentage tolerance ")
 combo2 = Combobox(window)
 combo2['values']= (1, 5, 10, 15, 20)
 combo2.current(1) #set the selected item
 
-# input 3
+# value 3
 lbl3a = Label(window, text="Reference for input 3 ")
 txt3 = Entry()
 txt3.insert(END, '1000')
 txt3.pack()
-lbl3b = Label(window, text="     Percentage range ")
+lbl3b = Label(window, text="     Percentage tolerance ")
 combo3 = Combobox(window)
 combo3['values']= (1, 5, 10, 15, 20)
 combo3.current(1) #set the selected item
 
-# input 4
+# value 4
 lbl4a = Label(window, text="Reference for input 4 ")
 txt4 = Entry()
 txt4.insert(END, '10000')
 txt4.pack()
-lbl4b = Label(window, text="     Percentage range ")
+lbl4b = Label(window, text="     Percentage tolerance ")
 combo4 = Combobox(window)
 combo4['values']= (1, 5, 10, 15, 20)
 combo4.current(1) #set the selected item
 
-
-
-
-
+# inputs
+lblin1 = Label(window, text="     Input test value 1 ")
+txtin1 = Entry()
+txtin1.insert(END, '10')
+txtin1.pack()
+lblin2 = Label(window, text="     Input test value 2 ")
+txtin2 = Entry()
+txtin2.insert(END, '100')
+txtin2.pack()
+lblin3 = Label(window, text="     Input test value 3 ")
+txtin3 = Entry()
+txtin3.insert(END, '1000')
+txtin3.pack()
+lblin4 = Label(window, text="     Input test value 4 ")
+txtin4 = Entry()
+txtin4.insert(END, '10000')
+txtin4.pack()
 
 
 
@@ -120,11 +181,29 @@ txt4.grid(column=1, row=3)
 lbl4b.grid(column=2, row=3)
 combo4.grid(column=3, row=3)
 
-#Button settings
-btn = Button(window, text="Measure", command=clicked)
+lblin1.grid(column=4, row=0)
+txtin1.grid(column=5, row=0)
+lblin2.grid(column=4, row=1)
+txtin2.grid(column=5, row=1)
+lblin3.grid(column=4, row=2)
+txtin3.grid(column=5, row=2)
+lblin4.grid(column=4, row=3)
+txtin4.grid(column=5, row=3)
+
+# instructionss
+btnins = Button(window, text="instructions", command=instructions)
+btnins.grid(column=1, row=7)
+
+# set tolerance button
+btn = Button(window, text="Set tolerance", command=set_tolerance)
 btn.grid(column=2, row=7)
 
+# checkrange button
+btn2 = Button(window, text="Check data", command=checkRange)
+btn2.grid(column=5, row=7)
 
+
+#update results
 
 
 window.mainloop()
